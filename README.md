21c6c589805797b233010f9b50b840c923f0fcd8
johnloberger <johnloberger@gmail.com>
2020-09-24 00:16:48 -0500

    formatting and ReadMe update

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# FetchRewardsCodingExercise_BE-SE
+<!-- PROJECT SHIELDS -->
+
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE">
+    <img src="https://images.squarespace-cdn.com/content/v1/59d39a7037c581d0d49cd94f/1565285374265-PIP0ZQDLD0OUDU5VL43B/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH-2yKxPTYak0SCdSGNKw8A2bnS_B4YtvNSBisDMT-TGt1lH3P2bFZvTItROhWrBJ0/Threadmark+-+Cover.gif" alt="Logo">
+  </a>
+
+  <h3 align="center">Welcome to Version Analyzer 1.0!</h3>
+
+  <p align="center">
+    This React single-page application is designed to allow individuals to compare any two software version numbers. 
+    <br />
+    <br />
+    ·
+    <a href="https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE/issues">Request Feature</a>
+  </p>
+</p>
+
+### Built With
+This single-page application was created entirely with React.js
+* [React](https://reactjs.org/)
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To setup and run this project locally, please complete the following instructions.
+
+### Prerequisites
+
+You should know how to setup basic React apps and use NPM.
+
+### Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE.git
+```
+2. CD into the directory
+```sh
+cd FetchRewardsCodingExercise_BE-SE/
+```
+3. Install NPM packages on the directory and start the server!
+```sh
+npm install && npm start
+```
+4. Enjoy the application
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+To use this application, simply enter any two variations of a software version such as '1' or '1.2' or '1.2.3' and click the 'Compare' button.
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- CONTACT -->
+## Contact
+
+John Loberger - [My Website](https://jloberger.com) - johnloberger@gmail.com
+
+Project Link: [https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE](https://github.com/johnloberger/Clyma-Frontend)
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[forks-shield]: https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE.svg?style=flat-square
+[forks-url]: https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE/network/members
+[stars-shield]: https://img.shields.io/github/stars/johnloberger/FetchRewardsCodingExercise_BE-SE.svg?style=flat-square
+[stars-url]: https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE/stargazers
+[issues-shield]: https://img.shields.io/github/issues/johnloberger/FetchRewardsCodingExercise_BE-SE.svg?style=flat-square
+[issues-url]: https://github.com/johnloberger/FetchRewardsCodingExercise_BE-SE/issues
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/johnloberger
--- src/CompareVersions.js
+++ src/CompareVersions.js
@@ -4,16 +4,16 @@ class CompareVersions extends React.Component {
   state = {
     Version1: "",
     Version2: "",
-    comparisonResult: false
+    comparisonResult: false,
   };
 
-  shouldComponentUpdate(nextProps, nextState){
-    if (this.state.Version1 !== nextState.Version1){
-      return false
-    } else if (this.state.Version2 !== nextState.Version2){
-      return false
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.Version1 !== nextState.Version1) {
+      return false;
+    } else if (this.state.Version2 !== nextState.Version2) {
+      return false;
     }
-    return true
+    return true;
   }
 
   handleChange = (event) => {
@@ -25,9 +25,12 @@ class CompareVersions extends React.Component {
   handleFormSubmit = (event) => {
     event.preventDefault();
     this.setState({
-      comparisonResult: this.compareVersions(this.state.Version1, this.state.Version2)
-    })
-  }
+      comparisonResult: this.compareVersions(
+        this.state.Version1,
+        this.state.Version2
+      ),
+    });
+  };
 
   compareVersions = (version1, version2) => {
     let current = version1
@@ -49,55 +52,69 @@ class CompareVersions extends React.Component {
   };
 
   render() {
-    let equal = <span style={{color: '#03fcf4'}}>equal</span>
-    let after = <span style={{color: '#ff3d3d'}}>after</span>
-    let before = <span style={{color: '#3dff5a'}}>before</span>
+    let equal = <span style={{ color: "#03fcf4" }}>equal</span>;
+    let after = <span style={{ color: "#ff3d3d" }}>after</span>;
+    let before = <span style={{ color: "#3dff5a" }}>before</span>;
     return (
-      <div style={{top: '8%', position: 'absolute'}}>
-      <div class="prompt">
-        <h3>Welcome to Version Analyzer 1.0</h3>
-        <p style={{ color: 'rgba(255, 255, 255, 0.5)'}}>Accepted Formats:  '1' -- '1.2' -- '1.2.3'</p>
-        <div class="form-container">
-          <section>
-            <form onSubmit={this.handleFormSubmit} class="compare-form">
-              <label>
-                <input
-                  onChange={this.handleChange}
-                  type="text"
-                  pattern="[+]?[0-9]*[.]?[0-9]?[0-9]*[.]?[0-9]*[.]?[0-9]+"
-                  name="Version1"
-                  id="version1"
-                  autoComplete="off"
-                  required
-                />
-                <div class="label-text">Version A</div>
-              </label>
-              <label>
-                <input
-                  onChange={this.handleChange}
-                  type="text"
-                  name="Version2"
-                  id="version2"
-                  autoComplete="off"
-
-                  required
-                />
-                <div class="label-text">Version B</div>
-              </label>
-              <br></br>
-              <button class="compare-btn" type="submit" value="Submit">
-                Compare
-              </button>
-              <br></br>
-            </form>
-          </section>
+      <div style={{ top: "8%", position: "absolute" }}>
+        <div class="prompt">
+          <h3>Welcome to Version Analyzer 1.0</h3>
+          <p style={{ color: "rgba(255, 255, 255, 0.5)" }}>
+            Accepted Formats: '1' -- '1.2' -- '1.2.3'
+          </p>
+          <div class="form-container">
+            <section>
+              <form onSubmit={this.handleFormSubmit} class="compare-form">
+                <label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    pattern="[+]?[0-9]*[.]?[0-9]?[0-9]*[.]?[0-9]*[.]?[0-9]+"
+                    name="Version1"
+                    id="version1"
+                    autoComplete="off"
+                    required
+                  />
+                  <div class="label-text">Version A</div>
+                </label>
+                <label>
+                  <input
+                    onChange={this.handleChange}
+                    type="text"
+                    name="Version2"
+                    id="version2"
+                    autoComplete="off"
+                    required
+                  />
+                  <div class="label-text">Version B</div>
+                </label>
+                <br></br>
+                <button class="compare-btn" type="submit" value="Submit">
+                  Compare
+                </button>
+                <br></br>
+              </form>
+            </section>
+          </div>
         </div>
-      </div>
-      <div>
-        {this.state.comparisonResult === 0 ? <h2>Version {this.state.Version1} is {equal} to Version {this.state.Version2}</h2>
-        : this.state.comparisonResult === 1 ? <h2>Version {this.state.Version1} was made {after} Version {this.state.Version2}</h2>
-        : this.state.comparisonResult === -1 ? <h2>Version {this.state.Version1} was made {before} Version {this.state.Version2}</h2> : null}
-      </div>
+        <div>
+          {this.state.comparisonResult === 0 ? (
+            <h2>
+              Version {this.state.Version1} is {equal} to Version{" "}
+              {this.state.Version2}
+            </h2>
+          ) : this.state.comparisonResult === 1 ? (
+            <h2>
+              Version {this.state.Version1} was made {after} Version{" "}
+              {this.state.Version2}
+            </h2>
+          ) : this.state.comparisonResult === -1 ? (
+            <h2>
+              Version {this.state.Version1} was made {before} Version{" "}
+              {this.state.Version2}
+            </h2>
+          ) : null}
+        </div>
       </div>
     );
   }
